21c27b7a19472ecdb2dcedc0e65e98311ccd526b
surbhi chouhan <surbhichouhan200@gmail.com>
2021-04-11 23:15:41 +0530

    first commit after creating app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
+const img1 = "https://picsum.photos/250/300"
+const img2 = "https://picsum.photos/275/300"
+const img3 = "https://picsum.photos/300/300"
+const img4 = "https://picsum.photos/350/300"
+
+const heading = {
+    color : "blue",
+    textAlign : "center",
+    textTransform : "capitalize",
+    textShadow : "1px 2px 3px blueviolet",
+    margin : "100px 0px",
+    fontFamily : "'Lobster', cursive"
+  }
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+        <h1 style={heading}>My name is sunil shrimali.</h1>
+        <div class="img_div">
+            <img src={img1} alt="random picture" />
+            <img src={img2} alt="random picture" />
+            <img src={img3} alt="random picture" />
+            <img src={img4} alt="random picture" />
+        </div>
+    </>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
